1429ad0f081b37abe95bb4ee672fef177aa7e9d6
TheTrueShaman <68354378+The-Shaman@users.noreply.github.com>
2022-05-31 09:49:46 -0700

    Update index.js

--- tools/infobox-generator/js/index.js
+++ tools/infobox-generator/js/index.js
@@ -109,8 +109,8 @@ function create_infobox(itemData) {
     } else {
         infobox += 'item';
     }
-    infobox += '\n|rarity = ';
-    infobox += itemData['tier'].toLowerCase() + '\n';
+    infobox += '\n|rarity = ' + itemData['tier'].toLowerCase() + '\n';
+    infobox += '|id = ' + itemData['id'] + '\n';
     if (itemData['stats']) {
         const stat_keys = Object.keys(itemData['stats']);
         for (let i = 0; i < stat_keys.length; i++) {
