0ac16e5b6203f5c25c26c68392ba6f2861759274
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-06-05 12:13:34 -0400

    separate data files, added eslint config

---
+++ data/contributors.js
@@ -0,0 +1,102 @@
+export const contributors = [
+    {
+        name: 'joker876',
+        thumbnail: 'joker876.png',
+        aka: [],
+        tasks: ['Owner of the GitHub repository and organization', 'Created the main page', 'Helped create the Head Render Thingy', 'Helped create the Leather Armor Renderer'],
+        links: [
+            {
+                type: 'wiki',
+                value: 'https://hypixel-skyblock.fandom.com/wiki/User:Joker876',
+                border: true,
+            },
+            {
+                type: 'discord',
+                value: 'joker876#8914',
+            },
+            {
+                type: 'github',
+                value: 'https://github.com/joker876',
+            },
+            {
+                type: 'curseForge',
+                value: 'https://www.curseforge.com/members/joker876xd8',
+            },
+        ],
+    },
+    {
+        name: 'Fewfre',
+        thumbnail: 'fewfre.png',
+        aka: [],
+        tasks: ['Helped create the Head Render Thingy', 'Helped create the Leather Armor Renderer', 'Helped with many other issues'],
+        links: [
+            {
+                type: 'wiki',
+                value: 'https://hypixel-skyblock.fandom.com/wiki/User:Fewfre',
+                border: true,
+            },
+            {
+                type: 'discord',
+                value: 'fewfre#9549',
+            },
+            {
+                type: 'github',
+                value: 'https://github.com/fewfre',
+            },
+        ],
+    },
+    {
+        name: 'Thundercraft5',
+        thumbnail: 'ruby.png',
+        aka: ['Ruby'],
+        tasks: ['Owner of the GitHub repository and organization', 'Maintains the Hypixel SkyBlock Wiki Bot', 'Added various enhancements to the site', "<span style='color: #f33; margin-left: 4px;'>No longer active!</span>"],
+        links: [
+            {
+                type: 'wiki',
+                value: 'https://hypixel-skyblock.fandom.com/wiki/User:Thundercraft5',
+                border: true,
+            },
+            {
+                type: 'discord',
+                value: '💻Ruby#3280',
+            },
+            {
+                type: 'github',
+                value: 'https://github.com/Thundercraft5',
+            },
+        ],
+    },
+    {
+        name: 'MonkeysHK',
+        thumbnail: 'blank.png',
+        aka: [],
+        tasks: ['Member of the GitHub repository/organization', 'Helped create the Head Render Thingy', 'Helped create the Leather Armor Renderer'],
+        links: [
+            {
+                type: 'wiki',
+                value: 'https://hypixel-skyblock.fandom.com/wiki/User:MonkeysHK',
+                border: true,
+            },
+            {
+                type: 'github',
+                value: 'https://github.com/MonkeysHK',
+            },
+            {
+                type: 'curseForge',
+                value: 'https://www.curseforge.com/members/MonkeysHK',
+            },
+        ],
+    },
+    {
+        name: 'Pwign',
+        thumbnail: 'pwign.png',
+        aka: [],
+        tasks: ['Helped create the Head Render Thingy', "<span style='color: #f33; margin-left: 4px;'>No longer active!</span>"],
+        links: [
+            {
+                type: 'wiki',
+                value: 'https://hypixel-skyblock.fandom.com/wiki/User:Pwign',
+            },
+        ],
+    },
+];
