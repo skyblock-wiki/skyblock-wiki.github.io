0c6f6fb2e9a7cd31c192b816683239072388b2d3
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-06-06 08:25:01 -0400

    global contributors script

---
+++ scripts/contributors.js
@@ -0,0 +1,39 @@
+import { contributors } from '../data/page-contributors.js';
+
+const tool = window.location.pathname.replace(/\/tools\/|\/|index\.html/g, '');
+
+if (tool in contributors) {
+    const contributorsList = Object.keys(contributors[tool]).map((name) => createContributor(name, contributors[tool][name]));
+
+    document.getElementById('contrib-list').innerHTML = contributorsList.join('');
+
+    document.getElementById('contrib-list-div').style.display = 'none';
+
+    document.getElementById('show-contributors').addEventListener('click', () => {
+        document.getElementById('contrib-list-div').classList.toggle('hidden-height');
+        $('#contrib-list-div').toggle('slow');
+    });
+}
+
+/**
+ * Creates information for a contributor
+ * @param {string} name the name of the contributor
+ * @param {Array} list the list of tasks the contributor has done
+ * @returns {string} the HTML string for the contributor
+ */
+function createContributor(name, list) {
+    return [
+        '<li>', //
+        '<div class="tooltip">',
+        '<span>',
+        name,
+        '</span>',
+        '<span class="tooltip-text">',
+        '<ul>',
+        list.map((text) => `<li><span>${text}</span></li>`).join(''),
+        '</ul>',
+        '</span>',
+        '</div>',
+        '</li>',
+    ].join('');
+}
