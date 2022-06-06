cd6f3d6989ef465902b00e3a342b444301b401b0
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-06-05 22:29:29 -0400

    updated mcstacker logo, formatter contrib/download data

--- data/contributors.js
+++ data/contributors.js
@@ -5,23 +5,10 @@ export const contributors = [
         owner: true,
         tasks: ['Created the main page', 'Helped create the Head Render Thingy', 'Helped create the Leather Armor Renderer'],
         links: [
-            {
-                type: 'wiki',
-                value: 'https://hypixel-skyblock.fandom.com/wiki/User:Joker876',
-                border: true,
-            },
-            {
-                type: 'discord',
-                value: 'joker876#8914',
-            },
-            {
-                type: 'github',
-                value: 'https://github.com/joker876',
-            },
-            {
-                type: 'curseForge',
-                value: 'https://www.curseforge.com/members/joker876xd8',
-            },
+            { type: 'wiki', value: 'https://hypixel-skyblock.fandom.com/wiki/User:Joker876', border: true },
+            { type: 'discord', value: 'joker876#8914' },
+            { type: 'github', value: 'https://github.com/joker876' },
+            { type: 'curseForge', value: 'https://www.curseforge.com/members/joker876xd8' },
         ],
     },
     {
@@ -29,19 +16,9 @@ export const contributors = [
         thumbnail: 'fewfre',
         tasks: ['Helped create the Head Render Thingy', 'Helped create the Leather Armor Renderer', 'Helped with many other issues'],
         links: [
-            {
-                type: 'wiki',
-                value: 'https://hypixel-skyblock.fandom.com/wiki/User:Fewfre',
-                border: true,
-            },
-            {
-                type: 'discord',
-                value: 'fewfre#9549',
-            },
-            {
-                type: 'github',
-                value: 'https://github.com/fewfre',
-            },
+            { type: 'wiki', value: 'https://hypixel-skyblock.fandom.com/wiki/User:Fewfre', border: true },
+            { type: 'discord', value: 'fewfre#9549' },
+            { type: 'github', value: 'https://github.com/fewfre' },
         ],
     },
     {
@@ -50,19 +27,9 @@ export const contributors = [
         member: true,
         tasks: ['Helped create the Head Render Thingy', 'Helped create the Leather Armor Renderer'],
         links: [
-            {
-                type: 'wiki',
-                value: 'https://hypixel-skyblock.fandom.com/wiki/User:MonkeysHK',
-                border: true,
-            },
-            {
-                type: 'github',
-                value: 'https://github.com/MonkeysHK',
-            },
-            {
-                type: 'curseForge',
-                value: 'https://www.curseforge.com/members/MonkeysHK',
-            },
+            { type: 'wiki', value: 'https://hypixel-skyblock.fandom.com/wiki/User:MonkeysHK', border: true },
+            { type: 'github', value: 'https://github.com/MonkeysHK' },
+            { type: 'curseForge', value: 'https://www.curseforge.com/members/MonkeysHK' },
         ],
     },
     {
@@ -71,19 +38,9 @@ export const contributors = [
         member: true,
         tasks: ['Helped create the Infobox Generator'],
         links: [
-            {
-                type: 'wiki',
-                value: 'https://hypixel-skyblock.fandom.com/wiki/User:TheTrueShaman',
-                border: true,
-            },
-            {
-                type: 'discord',
-                value: 'TheTrueShaman#9445',
-            },
-            {
-                type: 'github',
-                value: 'https://github.com/The-Shaman',
-            },
+            { type: 'wiki', value: 'https://hypixel-skyblock.fandom.com/wiki/User:TheTrueShaman', border: true },
+            { type: 'discord', value: 'TheTrueShaman#9445' },
+            { type: 'github', value: 'https://github.com/The-Shaman' },
         ],
     },
     {
@@ -92,19 +49,9 @@ export const contributors = [
         member: true,
         tasks: ['Helped with various things in the site', 'Contributed to the Hypixel SkyBlock Wiki Bot'],
         links: [
-            {
-                type: 'wiki',
-                value: 'https://hypixel-skyblock.fandom.com/wiki/User:Eejit43',
-                border: true,
-            },
-            {
-                type: 'discord',
-                value: 'eejit#7578',
-            },
-            {
-                type: 'github',
-                value: 'https://github.com/Eejit43',
-            },
+            { type: 'wiki', value: 'https://hypixel-skyblock.fandom.com/wiki/User:Eejit43', border: true },
+            { type: 'discord', value: 'eejit#7578' },
+            { type: 'github', value: 'https://github.com/Eejit43' },
         ],
     },
     {
@@ -114,19 +61,9 @@ export const contributors = [
         tasks: ['Owner of the GitHub repository and organization', 'Contributed to the Hypixel SkyBlock Wiki Bot', 'Added various enhancements to the site'],
         inactive: true,
         links: [
-            {
-                type: 'wiki',
-                value: 'https://hypixel-skyblock.fandom.com/wiki/User:Thundercraft5',
-                border: true,
-            },
-            {
-                type: 'discord',
-                value: '💻Ruby#3280',
-            },
-            {
-                type: 'github',
-                value: 'https://github.com/Thundercraft5',
-            },
+            { type: 'wiki', value: 'https://hypixel-skyblock.fandom.com/wiki/User:Thundercraft5', border: true },
+            { type: 'discord', value: '💻Ruby#3280' },
+            { type: 'github', value: 'https://github.com/Thundercraft5' },
         ],
     },
     {
@@ -134,11 +71,6 @@ export const contributors = [
         thumbnail: 'pwign',
         tasks: ['Helped create the Head Render Thingy'],
         inactive: true,
-        links: [
-            {
-                type: 'wiki',
-                value: 'https://hypixel-skyblock.fandom.com/wiki/User:Pwign',
-            },
-        ],
+        links: [{ type: 'wiki', value: 'https://hypixel-skyblock.fandom.com/wiki/User:Pwign' }],
     },
 ];
--- data/downloads.js
+++ data/downloads.js
@@ -4,15 +4,8 @@ export const downloads = [
         description: 'A mod that lets you render any entity (mob, player, NPC, item, anything) with a single click of a button. Requires Forge for Minecraft 1.8.x.',
         thumbnail: 'wikitools',
         links: [
-            {
-                type: 'download',
-                link: 'wikitools-2.6.5.jar',
-                border: true,
-            },
-            {
-                type: 'github',
-                link: 'https://github.com/Charzard4261/wikitools',
-            },
+            { type: 'download', link: 'wikitools-2.6.5.jar', border: true },
+            { type: 'github', link: 'https://github.com/Charzard4261/wikitools' },
         ],
     },
     {
@@ -21,19 +14,9 @@ export const downloads = [
         thumbnail: 'mineshot',
         largeThumbnail: true,
         links: [
-            {
-                type: 'download',
-                link: 'Mineshot-v1.7-1.8.9.jar',
-                border: true,
-            },
-            {
-                type: 'website',
-                link: 'http://www.9minecraft.net/mineshot-mod',
-            },
-            {
-                type: 'github',
-                link: 'https://github.com/ata4/mineshot',
-            },
+            { type: 'download', link: 'Mineshot-v1.7-1.8.9.jar', border: true },
+            { type: 'website', link: 'http://www.9minecraft.net/mineshot-mod' },
+            { type: 'github', link: 'https://github.com/ata4/mineshot' },
         ],
     },
     {
@@ -41,16 +24,8 @@ export const downloads = [
         description: 'A mod that lets you download a world from multiplayer to singleplayer.',
         thumbnail: 'world-downloader',
         links: [
-            {
-                type: 'text',
-                link: 'https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/minecraft-mods/2520465-world-downloader-mod-create-backups-of-your-builds#Downloads',
-                text: 'Choose Version',
-                border: true,
-            },
-            {
-                type: 'github',
-                link: 'https://github.com/Pokechu22/WorldDownloader/releases',
-            },
+            { type: 'text', text: 'Choose Version', link: 'https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/minecraft-mods/2520465-world-downloader-mod-create-backups-of-your-builds#Downloads', border: true },
+            { type: 'github', link: 'https://github.com/Pokechu22/WorldDownloader/releases' },
         ],
     },
     {
@@ -58,16 +33,8 @@ export const downloads = [
         description: 'A set of <span class="code">.psd</span> templates used to make images for enchanted items and blocks.',
         thumbnail: 'enchanted-items-ps',
         links: [
-            {
-                type: 'download',
-                link: 'Enchantment_Glint_ps.zip',
-                border: true,
-            },
-            {
-                type: 'text',
-                link: 'https://www.photoshop.com/en',
-                text: 'Get Photoshop',
-            },
+            { type: 'download', link: 'Enchantment_Glint_ps.zip', border: true },
+            { type: 'text', text: 'Get Photoshop', link: 'https://www.photoshop.com/en' },
         ],
     },
     {
@@ -75,16 +42,8 @@ export const downloads = [
         description: 'A set of <span class="code">.xcf</span> templates used to make images for enchanted items and blocks.',
         thumbnail: 'enchanted-items-gimp',
         links: [
-            {
-                type: 'download',
-                link: 'Enchantment_Glint_gimp.zip',
-                border: true,
-            },
-            {
-                type: 'text',
-                link: 'https://www.gimp.org/',
-                text: 'Get GIMP',
-            },
+            { type: 'download', link: 'Enchantment_Glint_gimp.zip', border: true },
+            { type: 'text', text: 'Get GIMP', link: 'https://www.gimp.org' },
         ],
     },
     {
@@ -92,19 +51,9 @@ export const downloads = [
         description: 'A mod that allows quick rendering of any item, block and head.',
         thumbnail: 'blockrenderer',
         links: [
-            {
-                type: 'download',
-                link: 'BlockRenderer-0.3.3.jar',
-                border: true,
-            },
-            {
-                type: 'curseForge',
-                link: 'https://www.curseforge.com/minecraft/mc-mods/blockrenderer',
-            },
-            {
-                type: 'github',
-                link: 'https://github.com/unascribed/BlockRenderer',
-            },
+            { type: 'download', link: 'BlockRenderer-0.3.3.jar', border: true },
+            { type: 'curseForge', link: 'https://www.curseforge.com/minecraft/mc-mods/blockrenderer' },
+            { type: 'github', link: 'https://github.com/unascribed/BlockRenderer' },
         ],
     },
 ];
